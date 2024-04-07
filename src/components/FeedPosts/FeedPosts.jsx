b8ed375622772af0394dda925843c8541f559f13
IsraelOho <151618401+ohoisrael@users.noreply.github.com>
2024-04-07 14:38:23 +0000

    Update FeedPosts.jsx

--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -27,7 +27,7 @@ const FeedPosts = () => {
 			{!isLoading && posts.length === 0 && (
 				<>
 					<Text fontSize={"md"} color={"red.400"}>
-						Dayuum. Looks like you don&apos;t have any friends.
+						Hi there, Welcome. Looks like you don&apos;t have any friends.
 					</Text>
 					<Text color={"red.400"}>Start following people and posting!!</Text>
 				</>
