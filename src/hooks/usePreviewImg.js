cf84e96ec7d78defa57fcc3ee6440524338390e6
IsraelOho <151618401+ohoisrael@users.noreply.github.com>
2024-05-30 16:56:46 +0000

    Update usePreviewImg.js

--- src/hooks/usePreviewImg.js
+++ src/hooks/usePreviewImg.js
@@ -4,13 +4,13 @@ import useShowToast from "./useShowToast";
 const usePreviewImg = () => {
 	const [selectedFile, setSelectedFile] = useState(null);
 	const showToast = useShowToast();
-	const maxFileSizeInBytes = 2 * 1024 * 1024; // 2MB
+	const maxFileSizeInBytes = 10 * 1024 * 1024; // 10MB
 
 	const handleImageChange = (e) => {
 		const file = e.target.files[0];
 		if (file && file.type.startsWith("image/")) {
 			if (file.size > maxFileSizeInBytes) {
-				showToast("Error", "File size must be less than 2MB", "error");
+				showToast("Error", "File size must be less than 10MB", "error");
 				setSelectedFile(null);
 				return;
 			}
